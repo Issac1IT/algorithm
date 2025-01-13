4e67fbedd8202273b774540b0c374bed5ab37a62
Issac1IT <151382167+Issac1IT@users.noreply.github.com>
2025-01-14 00:26:56 +0800

    Create Hello.cpp

---
+++ Hello.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+namespace one
+{
+    const int M=200;
+    int inf=10000;
+}
+namespace two
+{
+    int M=10;
+    int inf=-10000;
+}
+//first
+using namespace one;
+int main()
+{
+  cout<<M<<end1;
+  two::inf*=2;
+  cout<<two::inf<<end1;
+  using two::M;
+  cout<<
+  
+  return 0;
+}
